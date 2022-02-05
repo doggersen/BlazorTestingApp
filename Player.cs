fe4dc992b5b09259abc75075dafc8b6da3eeb5a5
doggersen <martin.mbj.johansen@gmail.com>
2022-02-05 15:35:06 +0100

    converting lists to my own lists.

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorTestingApp
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Point { get; set; } = 60;
+
+        public int Plads { get; set; }
+        public int SkaeveAccumulated { get; set; }
+
+        //automatic ID number
+        private static int m_Counter = 0;
+        public int Id { get; set; }
+
+
+        public Player(string name = "Anonymous")
+        {
+            Name = name;
+
+            this.Id = System.Threading.Interlocked.Increment(ref m_Counter); //part of the automatic ID number!
+        }
+
+
+    }
+}
